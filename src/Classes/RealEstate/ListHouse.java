2e23a85751a299adc4961ce9b9edd538e046b830
Disni Charuka <disnicharuka@gmail.com>
2015-03-25 10:43:43 +0530

    Implemented Listable interface in ListHouse class.
    Overrided the copy and compareTo methods.

--- src/Classes/RealEstate/ListHouse.java
+++ src/Classes/RealEstate/ListHouse.java
@@ -1,5 +1,23 @@
 package Classes.RealEstate;
 
-public class ListHouse {
+public class ListHouse implements Listable {
 
+    
+    public ListHouse(String lastName, String firstName, int lotNumber, int price, int squareFeet, int bedRooms) {
+       
+    }
+
+    @Override
+    public Listable copy() {
+        
+        return null;
+    }
+
+    @Override
+    public int compareTo(Listable otherListHouse) {
+        
+        return 0;
+    }
+
+    
 }
