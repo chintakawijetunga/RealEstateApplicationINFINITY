f7496375d20dd44709aaf502f1dbd8a28d837121
rddealwis <rddealwis@gmail.com>
2015-03-25 14:13:07 +0530

    Created the List class (abstract).

--- src/Classes/RealEstate/List.java
+++ src/Classes/RealEstate/List.java
@@ -1,4 +1,25 @@
 package Classes.RealEstate;
 
 public abstract class List {
+    protected Listable[] houseList;
+    public int numOfItems;
+    protected int currentPosition;
+
+    public List(int maxItems) {
+        
+        numOfItems = 0;
+        houseList = new Listable[maxItems];
+    }
+
+    public int listLengthIs() {
+        return numOfItems;
+    }
+
+    public abstract boolean isThereHouse(Listable item);
+
+    public abstract Listable retrieveHouse(Listable item);
+
+    public abstract void insertHouse(Listable item);
+
+    public abstract void deleteHouse(Listable item);
 }
