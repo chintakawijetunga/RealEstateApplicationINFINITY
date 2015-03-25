324c86e70b516ea5e53126cf6dfb739f3a4af2bc
Disni Charuka <disnicharuka@gmail.com>
2015-03-25 12:23:57 +0530

    Coding of method bodies of ListHouse class.
    implemented copy(), compareTo() and the constructor

--- src/Classes/RealEstate/ListHouse.java
+++ src/Classes/RealEstate/ListHouse.java
@@ -2,21 +2,37 @@ package Classes.RealEstate;
 
 public class ListHouse implements Listable {
 
+    private String lastName;
+    private String firstName;
+    private int lotNumber;
+    private int price;
+    private int squareFeet;
+    private int bedRooms;
+    
     
     public ListHouse(String lastName, String firstName, int lotNumber, int price, int squareFeet, int bedRooms) {
        
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.lotNumber = lotNumber;
+        this.price = price;
+        this.squareFeet = squareFeet;
+        this.bedRooms = bedRooms;
     }
 
     @Override
     public Listable copy() {
         
-        return null;
+        ListHouse result = new ListHouse(lastName, firstName, lotNumber, price, squareFeet, bedRooms);
+        return result;
     }
 
     @Override
     public int compareTo(Listable otherListHouse) {
         
-        return 0;
+        ListHouse otherHouse = (ListHouse) otherListHouse;
+        int diff=this.lotNumber - otherHouse.lotNumber;
+        return diff;
     }
 
     
