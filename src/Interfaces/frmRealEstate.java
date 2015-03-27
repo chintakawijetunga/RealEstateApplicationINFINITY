ea3d4da96fb7923c66345c17ce81ba46bb690838
Anuja-Wijethunga <anujabc2013@gmail.com>
2015-03-27 21:27:26 +0530

    Modified frmRealEstate.
    Clear, Search and Close buttons in frmRealEstate.

--- src/Interfaces/frmRealEstate.java
+++ src/Interfaces/frmRealEstate.java
@@ -1,29 +1,15 @@
 package Interfaces;
 
 import Classes.General.Button;
-import Classes.RealEstate.HouseFile;
-import Classes.RealEstate.ListHouse;
-import Classes.RealEstate.SortedList;
-import java.io.File;
-import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import org.jb2011.lnf.beautyeye.ch3_button.BEButtonUI;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class frmRealEstate extends javax.swing.JInternalFrame {
 
     private static frmRealEstate instance;
-    private final String path= "file.xml";    
-    private static SortedList list = new SortedList();
-    private ListHouse house;
        
     public frmRealEstate() {
-        initComponents(); 
-        this.loadTheXMLFile();
+        initComponents();      
     }
     
     public static frmRealEstate GetInstance() 
@@ -255,11 +241,6 @@ public class frmRealEstate extends javax.swing.JInternalFrame {
             }
             public void mouseExited(java.awt.event.MouseEvent evt) {
                 jBtnPopulateMouseExited(evt);
-            }
-        });
-        jBtnPopulate.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBtnPopulateActionPerformed(evt);
             }
         });
 
@@ -527,17 +508,6 @@ public class frmRealEstate extends javax.swing.JInternalFrame {
         this.dispose();
     }//GEN-LAST:event_jBtnCloseActionPerformed
 
-    private void jBtnPopulateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnPopulateActionPerformed
-        if (list.listLengthIs() != 0) {
-            house = (ListHouse) list.getNextItem(false);
-            showHouse(house);
-
-            PopulateTheTable();
-        } else {
-            JOptionPane.showMessageDialog(rootPane, "No houses to be shown.");
-        }
-    }//GEN-LAST:event_jBtnPopulateActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBtnAdd;
@@ -567,76 +537,7 @@ public class frmRealEstate extends javax.swing.JInternalFrame {
     private javax.swing.JTextField jTxtSqFeet;
     // End of variables declaration//GEN-END:variables
 
-   private void loadTheXMLFile() 
-   {
-      ListHouse house;
-
-      try {
-         File fXmlFile = new File(path);
-         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-         Document doc = dBuilder.parse(fXmlFile);
-
-         doc.getDocumentElement().normalize();
-
-         
-         NodeList nList = doc.getElementsByTagName("House");
-         int listSize=nList.getLength();
-         
-         for (int temp = 0; temp < listSize; temp++) {
-
-            Node nNode = nList.item(temp);
-
-               //JOptionPane.showMessageDialog(rootPane,"\nCurrent Element :" + nNode.getNodeName());
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               house = HouseFile.getNextHouse(nNode);
-               list.insertHouse(house);
-               //			JOptionPane.showMessageDialog(rootPane,xmlValue);
-            }
-         }
-
-         
-      } catch (Exception e) {
-         JOptionPane.showMessageDialog(rootPane, e.getMessage().toString());
-      }    
-   }
    
-   private void PopulateTheTable() 
-   {
-      ListHouse house;
-      int count = 0;
-      list.resetHouseList();
-      Object[] columnNames = {"Lot Number", "First Name", "Last Name", "Price", "Square Feet", "No of Bedrooms"};
-      DefaultTableModel model = new DefaultTableModel(new Object[0][0], columnNames);
-      
-      while (count <= list.listLengthIs() - 1) 
-      {
-         Object[] o = new Object[6];
-         
-         house = (ListHouse) list.getNextItem(false);
-
-         o[0] = Integer.toString(house.lotNumber());
-         o[1] = (house.firstName());
-         o[2] = (house.lastName());
-         o[3] = (house.price());
-         o[4] = (house.squareFeet());
-         o[5] = (house.bedRooms());
-         model.addRow(o);
-         count++;
-      }
-      list.resetHouseList();
-      jTableEstateInfo.setModel(model);
-   }
-   
-   private void showHouse(ListHouse house) 
-   {
-      jTxtLotNo.setText(Integer.toString(house.lotNumber()));
-      jTxtFirstName.setText(house.firstName());
-      jTxtLastName.setText(house.lastName());
-      jTxtPrice.setText(Integer.toString(house.price()));
-      jTxtSqFeet.setText(Integer.toString(house.squareFeet()));
-      jTxtNoOfBedrooms.setText(Integer.toString(house.bedRooms()));
-   }
 
     
 }
